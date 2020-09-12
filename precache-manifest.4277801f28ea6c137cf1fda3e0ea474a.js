fac324fc0a2172fef6fd1c612cd0d2aa89ff3b23
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2020-09-12 23:37:48 +0200

    Add built site

---
+++ precache-manifest.4277801f28ea6c137cf1fda3e0ea474a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9f4d4c1dbcf965900a2ded5c9ec941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9108649129f4308f1de1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "adeb1d2581245e9e8e3f",
+    "url": "/static/js/2.77fe3048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77fe3048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9108649129f4308f1de1",
+    "url": "/static/js/main.39fb781f.chunk.js"
+  },
+  {
+    "revision": "c00fcb0bc72dd288683c",
+    "url": "/static/js/runtime-main.ec6f1641.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
