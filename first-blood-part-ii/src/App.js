72716adac7d40f934d6724a1d6ed2930b2ab68e4
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2020-09-14 02:52:50 +0200

    Add PoC

--- first-blood-part-ii/src/App.js
+++ first-blood-part-ii/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect }  from 'react';
 import './App.css';
+import API from './api';
+
+function Pic(props) {
+
+		return (
+						<img src={props.src} alt="Parking view" />
+		);
+}
+
+async function getPicData() {
+		const response = await API.get("view");
+		console.log("response:", response);
+		return response.data;
+
+		// return new Promise( (resolutionFunc,rejectionFunc) => {
+		// 		resolutionFunc({
+		// 				url: "https://www.ima-home.be/layout/uploads/2020/06/ima-slide1.jpg",
+		// 				message: "xxxxx",
+		// 		});
+		// });
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		const [state, setState] = useState({
+				picSrc: undefined,
+				firstPic: true,
+		});
+		
+		useEffect(() => {
+				const renewInterval = state.firstPic ? 0 : state.duration;
+				console.log('Setting ' + renewInterval + ' sec timeout to renew...');
+				const timer = setTimeout(() => {
+						getPicData().then(data => {
+								console.log(data);
+								const duration = (data.duration ? data.duration : 60);
+								setState({
+										picSrc: data.url,
+										firstPic: false,
+										duration: duration,
+								});
+								document.title = data.message;
+						});
+				}, renewInterval*1000);
+				// Clear timeout if the component is unmounted
+				return () => clearTimeout(timer);
+		});
+
+		return (
+						<div className="App">
+						<div className="App-header">
+						<Pic src={state.picSrc} />
+						</div>
+						</div>
+		);
+
 }
 
 export default App;
---
+++ first-blood-part-ii/src/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://ib9m7jp63g.execute-api.eu-central-1.amazonaws.com/dev/'
+});
