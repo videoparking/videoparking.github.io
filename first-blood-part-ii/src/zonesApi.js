22ddef07d7e0fdaabd83d9581c0948b062417f38
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2020-12-10 23:46:52 +0100

    Move zonesApi to use github raw user content

--- first-blood-part-ii/src/zonesApi.js
+++ first-blood-part-ii/src/zonesApi.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.PUBLIC_URL;
-// const BASE_URL = 'https://github.com/videoparking/zones/raw/main/videoparking_zones/data/v1/'; // Not working due to CORS
-
 export default axios.create({
-    baseURL: BASE_URL + '/zones-data/v1/'
+    baseURL: 'https://raw.githubusercontent.com/videoparking/zones/master/v1/',
 //      baseURL: 'https://httpbin.org/status/404'
 });
